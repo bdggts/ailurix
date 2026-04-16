93c9a0b684266543c79da6175938075a7ade83ed
bdggts <bdggts@gmail.com>
2026-04-16 10:40:16 +0530

    v9.1: LUNARIX card sprite = front-facing native warrior

--- android/app/build/generated/source/buildConfig/release/com/dominex/arena/BuildConfig.java
+++ android/app/build/generated/source/buildConfig/release/com/dominex/arena/BuildConfig.java
@@ -7,6 +7,6 @@ public final class BuildConfig {
   public static final boolean DEBUG = false;
   public static final String APPLICATION_ID = "com.dominex.arena";
   public static final String BUILD_TYPE = "release";
-  public static final int VERSION_CODE = 65;
-  public static final String VERSION_NAME = "9.0";
+  public static final int VERSION_CODE = 66;
+  public static final String VERSION_NAME = "9.1";
 }
