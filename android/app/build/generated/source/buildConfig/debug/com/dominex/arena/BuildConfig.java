cd0c1b10f32cff35be083b19475928c64c275af9
bdggts <bdggts@gmail.com>
2026-04-18 09:01:44 +0530

    v15.9.3: ROOT FIX - remove position:relative from result-screen, flex:1 now works properly

--- android/app/build/generated/source/buildConfig/debug/com/dominex/arena/BuildConfig.java
+++ android/app/build/generated/source/buildConfig/debug/com/dominex/arena/BuildConfig.java
@@ -7,6 +7,6 @@ public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
   public static final String APPLICATION_ID = "com.dominex.arena";
   public static final String BUILD_TYPE = "debug";
-  public static final int VERSION_CODE = 119;
-  public static final String VERSION_NAME = "15.9.2";
+  public static final int VERSION_CODE = 120;
+  public static final String VERSION_NAME = "15.9.3";
 }
