ead37482fcd162a5d259d48b1e15701b8827183a
bdggts <bdggts@gmail.com>
2026-04-21 18:01:16 +0530

    v15.13.4: Press Start 2P font global - entire game from splash to result screen

--- android/app/build/generated/source/buildConfig/debug/com/dominex/arena/BuildConfig.java
+++ android/app/build/generated/source/buildConfig/debug/com/dominex/arena/BuildConfig.java
@@ -7,6 +7,6 @@ public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
   public static final String APPLICATION_ID = "com.dominex.arena";
   public static final String BUILD_TYPE = "debug";
-  public static final int VERSION_CODE = 159;
-  public static final String VERSION_NAME = "15.13.3";
+  public static final int VERSION_CODE = 160;
+  public static final String VERSION_NAME = "15.13.4";
 }
