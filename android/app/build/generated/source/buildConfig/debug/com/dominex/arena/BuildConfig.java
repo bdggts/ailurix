5cafee745a169b7b161a0bd96b553d9b3a7f754c
bdggts <bdggts@gmail.com>
2026-04-22 22:53:17 +0530

    v15.13.19: Fix scroll (touch-action:pan-y), remove duplicate CSS, clean MP lobby

--- android/app/build/generated/source/buildConfig/debug/com/dominex/arena/BuildConfig.java
+++ android/app/build/generated/source/buildConfig/debug/com/dominex/arena/BuildConfig.java
@@ -7,6 +7,6 @@ public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
   public static final String APPLICATION_ID = "com.dominex.arena";
   public static final String BUILD_TYPE = "debug";
-  public static final int VERSION_CODE = 174;
-  public static final String VERSION_NAME = "15.13.18";
+  public static final int VERSION_CODE = 175;
+  public static final String VERSION_NAME = "15.13.19";
 }
