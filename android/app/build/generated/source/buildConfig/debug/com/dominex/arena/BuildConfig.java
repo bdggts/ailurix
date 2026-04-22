cf3181481840ee4b5f146db070201ec4d33b95bf
bdggts <bdggts@gmail.com>
2026-04-22 23:12:55 +0530

    v15.13.20: Fix width overflow + JS scroll handler for mp-content bypass touch-action:none

--- android/app/build/generated/source/buildConfig/debug/com/dominex/arena/BuildConfig.java
+++ android/app/build/generated/source/buildConfig/debug/com/dominex/arena/BuildConfig.java
@@ -7,6 +7,6 @@ public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
   public static final String APPLICATION_ID = "com.dominex.arena";
   public static final String BUILD_TYPE = "debug";
-  public static final int VERSION_CODE = 175;
-  public static final String VERSION_NAME = "15.13.19";
+  public static final int VERSION_CODE = 176;
+  public static final String VERSION_NAME = "15.13.20";
 }
